c0dbe93e64d3ae91b146be42e77330d852c3137e
杨鹏博 <obpt@sina.com>
2019-01-16 21:35:37 +0800

    add button entry

---
+++ index.tsx
@@ -0,0 +1,8 @@
+import * as cui from "../src"
+import React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <cui.Button text="测试"></cui.Button>,
+    document.getElementById("container")
+);
