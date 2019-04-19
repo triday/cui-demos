a0b12af0fc9757df7e934123132434c7d4b6f592
杨鹏博 <obpt@sina.com>
2019-04-20 00:13:22 +0800

    feat: button demo

--- button.tsx
+++ button.tsx
@@ -4,14 +4,24 @@ import * as cui from ".."
 export class ButtonDemo extends React.Component<{}, {}>{
     render() {
         return (
-            <ul>
-                <li>
+            <div>
+                <h2>大按钮</h2>
+                <cui.ButtonList>
                     <cui.Button text="测试" click={() => cui.Dialog.showInfo('测试1')} ></cui.Button>
-                </li>
-                <li>
                     <cui.Button text="测试2" click={() => alert(2)} ></cui.Button>
-                </li>
-            </ul>
+                </cui.ButtonList>
+                <h2>中等按钮</h2>
+                <cui.ButtonList>
+                    <cui.Button text="测试" click={() => cui.Dialog.showInfo('测试1')} ></cui.Button>
+                    <cui.Button text="测试2" click={() => alert(2)} ></cui.Button>
+                </cui.ButtonList>
+                <h2>小按钮</h2>
+                <cui.ButtonList>
+                    <cui.Button text="测试" click={() => cui.Dialog.showInfo('测试1')} ></cui.Button>
+                    <cui.Button text="测试2" click={() => alert(2)} ></cui.Button>
+                </cui.ButtonList>
+            </div>
+
         )
     }
 }
